375bceb79a6286b32299cf2464e9bb506c418f55
dpandrader <dpandrader@gmail.com>
2022-09-21 19:18:51 -0500

    adds format

--- hospital_be/authApp/models/user.py
+++ hospital_be/authApp/models/user.py
@@ -1,18 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.contrib.auth.hashers import make_password
+
+
 class UserManager(BaseUserManager):
-  def create_user(self, username, password=None):
-    """
-    Creates and saves a user with the given username and password.
-    """
-    if not username:
-        raise ValueError('Users must have an username')
-        user = self.model(username=username)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-  def create_superuser(self, username, password):
+   def create_user(self, username, password=None):
+      """
+      Creates and saves a user with the given username and password.
+      """
+      if not username:
+          raise ValueError('Users must have an username')
+      user = self.model(username=username)
+      user.set_password(password)
+      user.save(using=self._db)
+      return user
+
+        
+   def create_superuser(self, username, password):
     """
     Creates and saves a superuser with the given username and password.
     """
